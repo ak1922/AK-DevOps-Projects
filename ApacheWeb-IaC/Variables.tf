e07cf30d099ff27e0c9f62cefb321aa0d2f4f386
Akwasi Owusu Akyaw <aoakyaw@protonmail.com>
2023-09-23 22:16:39 -0400

    Additions to variables

--- ApacheWeb-IaC/Variables.tf
+++ ApacheWeb-IaC/Variables.tf
@@ -10,38 +10,38 @@ variable "AvailZone-2" {
   default = "us-east-1f"
 }
 
-variable "AvailZone-3" {
-  description = "Third availability zone in us-east-1"
-  type = string
-  default = "us-east-1e"
-}
-
-variable "AvailZone-4" {
-  description = "Forth availability zone in us-east-1"
-  type = string
-  default = "us-east-1b"
-}
-
 variable "AvailZone-5" {
-  description = "Forth availability zone in us-east-1"
+  description = "Third availability zone in us-east-1"
   type = string
   default = "us-east-1c"
 }
 
 variable "AvailZone-6" {
-  description = "Sixth availability zone in us-east-1"
+  description = "Forth availability zone in us-east-1"
   type = string
   default = "us-east-1a"
 }
 
 variable "mykey" {
   default = "akKey"
+  description = "aws key pair to login to instance"
+  type = string
 }
 
 variable "amznlnx2" {
   default = "ami-01c647eace872fc02"
+  description = "Amazon machine image for aws linux hosts"
+  type = string
 }
 
 variable "instype" {
+  description = "Linux instance type"
+  type = string
   default = "t2.micro"
+}
+
+variable "LoadBalancerCert" {
+  description = "Https certificate"
+  type = string
+  default = "arn:aws:acm:us-east-1:355740888737:certificate/58496ee5-1ea8-4f1f-99e3-67a5fb7f5cfd"
 }
